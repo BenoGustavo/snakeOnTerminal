3d9802d9d50f4c8304f799f42669185c7097ef4e
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-09-16 18:02:53 -0300

    update snake method now has something

--- snake.py
+++ snake.py
@@ -14,7 +14,17 @@ class Snake:
         self.direction = self.DIRECTIONS["up"]
 
     def getSnakeBody(self):
+        """Returns a tuple with the coordinates of the snake tuple = (width,height)"""
         return self.snakeBody
 
     def updateSnake(self):
-        ...
+        """Updates the snake body on the board by an given direction"""
+
+        #Creates a new head based on the direction that was given by the user
+        newSnakeHead = self.snakeBody[0][0] + self.direction[0], self.snakeBody[0][1] + self.direction[1]
+
+        #Inserting the new head in the snakeBody tuple
+        self.snakeBody.insert(0,newSnakeHead)
+
+        #Removes the last segment of the snake
+        self.snakeBody.pop(-1)
