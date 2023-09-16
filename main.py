31b2862fbcc04ac7e9c74f364ea4e7cb6804380b
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-09-16 17:10:03 -0300

    getting the classes to run

---
+++ main.py
@@ -0,0 +1,12 @@
+from board import Board
+from snake import Snake
+from apple import Apple
+
+gameBoard = Board()
+mainSnake = Snake(gameBoard.getBoardSize()[1])
+apples = Apple()
+
+gameBoard.setSnakeInstance(mainSnake)
+gameBoard.setAppleInstance(apples)
+
+gameBoard.printBoard()
