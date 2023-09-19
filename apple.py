9d3aec8313c3259d795da65408e69e7555691455
BenoGustavo <gustavoleandrogorges@gmail.com>
2023-09-19 12:07:16 -0300

    new class variables, method to set up the snakeInstance inside the apple, added methods to get state of the apple, and method to update apple position after it is picked by the snake

--- apple.py
+++ apple.py
@@ -1,9 +1,30 @@
+from random import randint
+
+
 class Apple:
     def __init__(self) -> None:
-        self.applePosition = (7, 10)  # Testing
+        self.applePosition = (randint(1, 22), randint(1, 22))
+        self.isAppleEated = False
+
+        self.snakeInstance = None
+
+    def setSnakeInstance(self, snakeInstance):
+        self.snakeInstance = snakeInstance
 
     def getApplePosition(self):
         return self.applePosition
 
-    def updateApple(self):
-        ...
+    def setAppleToEated(self):
+        self.isAppleEated = True
+
+    def setAppleToUneated(self):
+        self.isAppleEated = False
+
+    def getAppleState(self):
+        return self.isAppleEated
+
+    def updateApplePosition(self):
+        self.applePosition = (randint(1, 22), randint(1, 22))
+        while self.applePosition in self.snakeInstance.getSnakeBody():
+            self.applePosition = (randint(1, 22), randint(1, 22))
+        return
